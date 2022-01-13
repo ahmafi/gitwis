5a44d894bb9af28aa65f12df94575859279c1481
Amir Hossein Mafi <amir77mafi@gmail.com>
2022-01-13 18:53:22 +0330

    client: initialize styled components with a dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,44 @@
 
 import React from 'react';
 import FilesList from './features/files/FilesList';
+import { ThemeProvider } from 'styled-components';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const darkTheme = {
+  colors: {
+    background: '#202124',
+    el1: '#303134',
+    primary: '#81d4fa',
+    primaryLight: '#b2fef7',
+    primaryDark: '#4ba3c7',
+    secondary: '',
+    secondaryLight: '',
+    secondaryDark: '',
+    text: {
+      background: '#bdc1c6',
+      el1: '#e8eaed',
+      el1Dark: '#444',
+      primary: '#000',
+      primaryLight: '#000',
+      primaryDark: '#000',
+      secondary: '#000',
+      secondaryLight: '#000',
+      secondaryDark: '#000',
+    },
+    hover: {
+      el1: '#404144',
+    },
+  },
+};
 
 export default function App() {
-  return <FilesList />;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <FilesList />
+    </ThemeProvider>
+  );
 }
